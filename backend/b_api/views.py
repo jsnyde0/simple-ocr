9a1db402f43c503ec7762819920af1b7c3b9980c
jsnyde0 <jonatansnyders@gmail.com>
2024-10-30 15:55:50 +0100

    echo back GET request

--- backend/b_api/views.py
+++ backend/b_api/views.py
@@ -1,4 +1,16 @@
+import json
 from django.http import JsonResponse
 
-def api_home(request):
-    return JsonResponse({"message": "Hi this is your API home!"})
+def api_home(request, *args, **kwargs):
+    data = {}
+    
+    try:
+        data = json.loads(request.body) # JSON.loads() converts JSON string to Python dictionary
+    except:
+        pass
+
+    data['url_params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+
+    return JsonResponse(data)
--- client/basic.py
+++ client/basic.py
@@ -2,9 +2,9 @@ import requests
 
 endpoint = "http://localhost:8000/api/v1/"
 
-response = requests.get(endpoint)
+response = requests.get(endpoint, params={"url_param": "test"}, json={"json_data": "test"})
 
-print(response.text)
+print(response.json())
 print("STATUS CODE:", response.status_code)
 
 
