79f2de114dc04b04741fb9ceeff3b0abe04c6129
jsnyde0 <jonatansnyders@gmail.com>
2024-10-31 17:54:47 +0100

    added Document to admin

--- backend/b_api/admin.py
+++ backend/b_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Document
 
 # Register your models here.
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'image', 'uploaded_at']
+
